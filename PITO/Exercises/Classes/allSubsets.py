03d0f141e3c28b6a8b5058c3018b7f071e7ebc52
GenisMC <genis9d@gmail.com>
2020-12-30 14:07:48 +0100

    Classes

---
+++ PITO/Exercises/Classes/allSubsets.py
@@ -0,0 +1,9 @@
+class solution:
+    def sub_sets(self,sset):
+        return self.subsetsRecur([],sorted(sset))
+    def subsetsRecur(self,current,sset):
+        if sset:
+            return self.subsetsRecur(current,sset[1:]) + self.subsetsRecur(current + [sset[0]],sset[1:])
+        return[current]
+print(solution().sub_sets([4,5,6]))
+#I honestly didn't undestand a thing
